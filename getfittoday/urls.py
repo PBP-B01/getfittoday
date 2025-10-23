d416b162ab0d39a74105804d3f3a5f5c4188ec14
AryNov <arya.nov.pratama@gmail.com>
2025-10-23 19:58:34 +0700

    blogs & events w/o styling and filter

--- getfittoday/urls.py
+++ getfittoday/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path("", include(("home.urls", "home"), namespace="home")),
     path("booking/", include(("booking.urls", "booking"), namespace="booking")),
     path("central/", include(("central.urls", "central"), namespace="central")),
-    path('store/', include('store.urls')),
+    path('store/', include('store.urls')), # app store
     path('community/', include('community.urls')),
     path("central/", include(("central.urls", "central"), namespace="central")),
+    path("blognevent/", include("BlognEvent.urls", namespace="BlognEvent"))
 ]
