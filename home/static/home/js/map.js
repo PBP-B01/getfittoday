036f7858e7da059637e8122ba632539d0ab478af
w7ldan <wildanhidayatirl@gmail.com>
2025-10-24 22:16:11 +0700

    Minor Changes

--- home/static/home/js/map.js
+++ home/static/home/js/map.js
@@ -275,7 +275,8 @@ async function updateUserLocationMarker(position) {
 
 function createCenterOnMeButton() {
     const controlButton = document.createElement("button");
-    controlButton.className = 'custom-map-control-button';
+    controlButton.className = 'map-top-right-button'; 
+    controlButton.id = 'center-on-me-btn'; 
     controlButton.innerHTML = `<svg width="24" height="24" viewBox="0 0 24 24"><path d="M12 8c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm8.94 3c-.46-4.17-3.77-7.48-7.94-7.94V1h-2v2.06C6.83 3.52 3.52 6.83 3.06 11H1v2h2.06c.46 4.17 3.77 7.48 7.94 7.94V23h2v-2.06c4.17-.46 7.48-3.77 7.94-7.94H23v-2h-2.06zM12 19c-3.87 0-7-3.13-7-7s3.13-7 7-7 7 3.13 7 7-3.13 7-7 7z"/></svg>`;
     controlButton.title = "Center map on my location";
 
@@ -291,7 +292,12 @@ function createCenterOnMeButton() {
         }
     });
 
-    map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(controlButton);
+    const mapControlsContainer = document.getElementById('map-controls-container');
+    const sidebarBtn = document.getElementById('sidebar-toggle-btn'); 
+    
+    if (mapControlsContainer && sidebarBtn) {
+        mapControlsContainer.insertBefore(controlButton, sidebarBtn);
+    } 
 }
 
 function highlightSpotCard(placeId) {
