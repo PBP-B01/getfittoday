3b54d369f5cbcd2d414a578608a64f8bc8ead12c
samindriano <samuelindriano@gmail.com>
2025-10-26 23:48:42 +0700

    update

--- BlognEvent/views.py
+++ BlognEvent/views.py
@@ -8,7 +8,6 @@ from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import login_required
 import json
 
-# === BLOG & EVENT PAGE ===
 def blogevent_page(request):
     is_admin = request.session.get("is_admin", False)
 
@@ -22,7 +21,6 @@ def blogevent_page(request):
     }
     return render(request, 'blogevent/blogevent_page.html', context)
 
-# === EVENT FORM PAGE ===
 @login_required
 def event_form_page(request):
     form = EventForm()
@@ -46,7 +44,6 @@ def event_form_page(request):
     }
     return render(request, 'blogevent/event_form.html', context)
 
-# === CREATE EVENT ===
 @login_required
 def create_event(request):
     if request.method == "POST":
@@ -62,7 +59,6 @@ def create_event(request):
             return redirect('BlognEvent:blogevent_page')
     return redirect('BlognEvent:event_form_page')
 
-# === EDIT EVENT ===
 @login_required
 def edit_event(request, event_id):
     event = get_object_or_404(Event, id=event_id)
@@ -105,7 +101,6 @@ def edit_event(request, event_id):
     }
     return render(request, 'blogevent/event_form.html', context)
 
-# === DELETE EVENT ===
 @login_required
 @require_POST
 def delete_event(request, event_id):
@@ -118,13 +113,11 @@ def delete_event(request, event_id):
     event.delete()
     return JsonResponse({'success': True})
 
-# === BLOG FORM PAGE ===
 @login_required
 def blog_form_page(request):
     form = BlogsForm()
     return render(request, 'blogevent/blog_form.html', {'form': form})
 
-# === CREATE BLOG ===
 @login_required
 def create_blog(request):
     if request.method == "POST":
@@ -136,7 +129,6 @@ def create_blog(request):
             return redirect('BlognEvent:blogevent_page')
     return redirect('BlognEvent:blog_form_page')
 
-# === EDIT BLOG ===
 @login_required
 def edit_blog(request, blog_id):
     blog = get_object_or_404(Blogs, id=blog_id)
@@ -155,7 +147,6 @@ def edit_blog(request, blog_id):
 
     return render(request, 'blogevent/blog_form.html', {'form': form, 'blog': blog})
 
-# === DELETE BLOG ===
 @login_required
 @require_POST
 def delete_blog(request, blog_id):
@@ -168,7 +159,6 @@ def delete_blog(request, blog_id):
     blog.delete()
     return JsonResponse({'success': True})
 
-# === EVENT DETAIL API ===
 def event_detail_api(request, event_id):
     event = get_object_or_404(Event, id=event_id)
     locations = [loc.name for loc in event.locations.all()]
@@ -187,7 +177,6 @@ def event_detail_api(request, event_id):
         'is_owner': is_owner or is_admin,
     })
 
-# === BLOG DETAIL API ===
 def blog_detail_api(request, blog_id):
     blog = get_object_or_404(Blogs, id=blog_id)
 
