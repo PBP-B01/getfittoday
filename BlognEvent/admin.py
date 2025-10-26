03a4cff561a93c062b5a6ccf3c15e76c37e5a627
w7ldan <wildanhidayatirl@gmail.com>
2025-10-26 19:32:56 +0700

    Minor Fixes

--- BlognEvent/admin.py
+++ BlognEvent/admin.py
@@ -3,28 +3,18 @@ from .models import Event, Blogs
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
-    list_display = ('name', 'user', 'starting_date', 'ending_date', 'event_type_display')
-
-    list_filter = ('starting_date', 'event_type')
-
+    list_display = ('name', 'user', 'starting_date', 'ending_date') 
+    list_filter = ('starting_date',) 
     search_fields = ('name', 'description')
-
     list_display_links = ('name',)
-
     fieldsets = (
         (None, {
-            'fields': ('name', 'description', 'user', 'event_type', 'image')
+            'fields': ('name', 'description', 'user', 'image') 
         }),
         ('Date and Location', {
             'fields': ('starting_date', 'ending_date', 'locations')
         }),
     )
-
-    def event_type_display(self, obj):
-        if hasattr(obj, 'event_type'):
-            return dict(Event.TYPE_EVENTS).get(obj.event_type, obj.event_type)
-        return 'N/A'
-    event_type_display.short_description = 'Type'
 
 @admin.register(Blogs)
 class BlogsAdmin(admin.ModelAdmin):
