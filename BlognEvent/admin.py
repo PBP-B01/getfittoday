eeea5e85d38f34538afd2bad657ca3f380682494
w7ldan <wildanhidayatirl@gmail.com>
2025-10-26 19:31:40 +0700

    Added admin.py

--- BlognEvent/admin.py
+++ BlognEvent/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from .models import Event, Blogs
 
-# Register your models here.
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'starting_date', 'ending_date', 'event_type_display')
+
+    list_filter = ('starting_date', 'event_type')
+
+    search_fields = ('name', 'description')
+
+    list_display_links = ('name',)
+
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'description', 'user', 'event_type', 'image')
+        }),
+        ('Date and Location', {
+            'fields': ('starting_date', 'ending_date', 'locations')
+        }),
+    )
+
+    def event_type_display(self, obj):
+        if hasattr(obj, 'event_type'):
+            return dict(Event.TYPE_EVENTS).get(obj.event_type, obj.event_type)
+        return 'N/A'
+    event_type_display.short_description = 'Type'
+
+@admin.register(Blogs)
+class BlogsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author')
+
+    search_fields = ('title', 'body')
+
+    list_display_links = ('title',)
+
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'author', 'body', 'image')
+        }),
+    )
